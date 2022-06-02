0207844d6894071684881e5453eb8eb0bca1f06d
Michael-champions <immichaelchampionss@gmail.com>
2022-06-02 07:32:02 -0700

    I am so tired right now

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print inputs here
+ * Return: always 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: 1%d byte (s)\n," sizeof(c));
+	printf("size of an int: 1%d byte (s)\n," sizeof(i));
+	printf("size of a long int: 1%d byte (s)\n," sizeof(li));
+	printf("size of a long long int: 1%d byte (s)\n" sizeof(lli);
+	printf("size of a float: 1%d byte (s)\n," sizeof(f));
+	return (0);
+}
