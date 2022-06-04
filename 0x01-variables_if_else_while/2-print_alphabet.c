d15cbaa7d80d1c02064521fd030772cad45c0002
Michael-champions <immichaelchampionss@gmail.com>
2022-06-04 09:02:32 -0700

    print a-z in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet a-z in lowercase
+ * Return: 0 if positive
+ *
+ */
+
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar("%c ", ch);
+		ch++;
+	}
+	return (0);
+}
