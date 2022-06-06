cd3cb92943188ed1ab467548662ba82ad854a6fc
Michael-champions <immichaelchampionss@gmail.com>
2022-06-06 08:00:59 -0700

    print single numbers from base 10 to 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main -print all sigle digit numbers of base 10 starting from 0, follow by \n
+ * Return: 0 if positive
+ *
+ */
+
+int main(void)
+{
+	int number;
+
+	number = n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+
+}
