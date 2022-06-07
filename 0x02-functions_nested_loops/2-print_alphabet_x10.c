5ea3fd6c1dc4f17f467f4420e5da54947de9eaac
Michael-champions <immichaelchampionss@gmail.com>
2022-06-07 06:47:18 -0700

    print alpabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -prints 10 times the alphabet, in lowercase, \n
+ *
+ * Return: 0 if true
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabet;
+	int alphabet1;
+
+	for (alphabet = 0; alphabet <= 9; alphabet++)
+	{
+		for (alphabet1 = 'a'; alphabet1 <= 'z'; alphabet1++)
+		{
+			_putchar(alphabet1);
+		}
+	}
+	_putchar(10);
+}
