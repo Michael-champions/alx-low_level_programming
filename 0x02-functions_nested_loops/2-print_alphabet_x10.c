7e0860a84a8255acc5022c3c56e060f70d9870dc
Michael-champions <immichaelchampionss@gmail.com>
2022-06-07 06:58:44 -0700

    print alpabet 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,12 @@ void print_alphabet_x10(void)
 	int alphabet;
 	int alphabet1;
 
-	for (alphabet = 0; alphabet <= 10; alphabet++)
+	for (alphabet = 0; alphabet <= 9; alphabet++)
 	{
 		for (alphabet1 = 'a'; alphabet1 <= 'z'; alphabet1++)
 		{
 			_putchar(alphabet1);
 		}
 	}
-	_putchar(10);
+	_putchar('\n');
 }
