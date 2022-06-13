7491ceb0c5e54ebaa07a524a0aa16452b90c05f0
Michael-champions <immichaelchampionss@gmail.com>
2022-06-13 06:35:38 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string being printed
+ *
+ * Return: always 0
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+	_putchar(10);
+}
