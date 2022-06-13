7ab6c45bf653397fe30815dcc380eebcb8f01ef2
Michael-champions <immichaelchampionss@gmail.com>
2022-06-13 05:50:28 -0700

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code for Holberton School students.
+ *
+ * @s: declaration of *s and paramters for the function _strlen
+ *
+ * Return: always 0
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+	{
+
+	}
+
+	return (0);
+}
