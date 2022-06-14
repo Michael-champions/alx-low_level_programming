d53475c57d2682bf2fb407fa675b79bf5c33ea32
Michael-champions <immichaelchampionss@gmail.com>
2022-06-14 08:44:14 -0700

    print revers string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,16 +10,11 @@
  */
 void print_rev(char *s)
 {
-	int len;
+	int i, n;
 
-	for (len = 0; *s != '\0'; len++)
-		s++;
-
-	for (len = len; len != 0; len--)
-		s--;
+	n = strlen(s);
+	for (i = n - 1; i => 0; i++)
 	{
-		_putchar(*s);
-		s--;
+		_putchar(s[i]);
 	}
-	_putchar('\n');
 }
