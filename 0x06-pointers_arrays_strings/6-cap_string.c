b446251b049cdd70351e2c2ec3c9e2163391149e
Michael-champions <immichaelchampionss@gmail.com>
2022-06-16 12:46:21 -0700

    capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stddef.h>
+
+/**
+ * cap_string - capitalize all string
+ *
+ * @n: pointer
+ *
+ * Return: always 0
+ */
+char *cap_string(char *n)
+{
+	int i;
+
+	i = 0;
+
+	if (n[0] >= 'a' && n[0] <= 'z')
+	{
+		n[0] = n[0] - 32;
+	}
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		switch (n[i])
+		{
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			case ' ':
+			case '10':
+			case '\t':
+				if (n[i + 1] > 96 && n[i + 1] < 123)
+				{
+					n[i + 1] = n[i + 1] - 32;
+				}
+		}
+	}
+	return (n);
+}
