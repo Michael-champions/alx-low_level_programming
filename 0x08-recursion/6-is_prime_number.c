d2ecce99c7f3ee76b07442af051abd42d3986fd6
Michael-champions <immichaelchampionss@gmail.com>
2022-06-23 15:16:40 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,13 +1,42 @@
 #include "main.h"
 
 /**
+ * prime2 - Makes possible to evaluate from 1 to n
+ * @a: same number as n
+ * @b: number that iterates from 1 to n
  *
- * _prime_number -> function that returns 1 if the input integer is a prime number
- * @n: input
+ * Return: On success 1.
+ */
+
+int prime2(int a, int b)
+{
+	if (a == b)
+	{
+		return (1);
+	}
+	else if (a % b == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (prime2(a, b + 1));
+	}
+}
+
+/**
+ * is_prime_number - checks if a number is prime returns 1
+ *
+ * if the input integer is a prime number
+ *
+ * @n: integer
  *
  * Return: sucess
  */
+
 int is_prime_number(int n)
 {
-
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
