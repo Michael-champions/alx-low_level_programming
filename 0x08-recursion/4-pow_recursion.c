aff331c2d9f87701b54be4fee385b6d5d7ae8805
Michael-champions <immichaelchampionss@gmail.com>
2022-06-23 13:10:56 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion
+ *
+ * @x: first input
+ *
+ * @y: second input
+ *
+ * Return: success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y <= 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1)
+	}
+	else 
+	{
+		return (*x _pow_recursion(x, y - 1));
+	}
+}
