4cb0dcfb316c1f3a0bbc6bd87b811d8f87a5ca1f
Michael-champions <immichaelchampionss@gmail.com>
2022-06-23 09:38:06 -0700

    tasks 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;
+	s = "jpa\0";
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
-	return (len);
+	if (s == '\0')
+		return (0);
+	s++;
+
+	return (1 + _strlen_recursion(s));
 }
