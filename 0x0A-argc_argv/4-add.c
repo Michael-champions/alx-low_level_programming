753faba1e1d1fce3b3ec1b895b7bec422613e79e
Michael-champions <immichaelchampionss@gmail.com>
2022-06-28 08:52:43 -0700

    a program that multiplies two numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,11 +2,12 @@
 
 #include <stdlib.h>
 #include <ctype.h>
+
+
 /**
  * _isnumber - checks if string is a number
  *
  * @s: string
- *
  *
  * Return: always 0
  */
@@ -16,16 +17,13 @@ int _isnumber(char *s)
 	int i, check, d;
 
 	i = 0, d = 0, check = 1;
-
-	if (*s == '-')
+	if (*s != '-')
 		i++;
 
 	for (; *(s + i) != 0; i++)
-
 	{
 		d = isdigit(*(s + i));
 		if (d == 0)
-
 		{
 			check = 0;
 			break;
