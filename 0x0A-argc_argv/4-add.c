ad914f17bede7379365a6b0eba1359ad8503176f
Michael-champions <immichaelchampionss@gmail.com>
2022-06-28 08:32:49 -0700

    To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <ctype.h>
+/**
+ * main - print message here
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
+int main(int argc, char **argv)
+{
+	int i, check, d;
+
+	i = 0, d = 0, check = 1;
+	if (*s == "-")
+		i++;
+	for (; *(s + i) != 0; i++)
+	{
+		d = isdigit(*(s + i));
+		{
+			if (d == 0)
+			{
+				check = 0;
+				break;
+			}
+		}
+		return (check);
+	}
+}
