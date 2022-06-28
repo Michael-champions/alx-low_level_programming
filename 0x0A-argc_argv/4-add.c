3a6298fb761d7977883de74e8b737edf16c61b66
Michael-champions <immichaelchampionss@gmail.com>
2022-06-28 09:06:19 -0700

    a program that multiplies two numbers.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,17 +13,22 @@
  * Return: If one of the numbers contains symbols that are non-digits - 1.
  * otherwise -0
  */
+
 int main(int argc, char *argv[])
 {
 	int num, digit, sum = 0;
 
 	for (num = 1; num < argc; num++)
+
 	{
+
 		for (digit = 0; argv[num][digit]; digit++)
+
 		{
 			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+
 			{
-				printf("error\n");
+				printf("Error\n");
 				return (1);
 			}
 		}
