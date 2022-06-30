98be6afc2086e8103bd6cad8985e3ee6d1618c44
Michael-champions <immichaelchampionss@gmail.com>
2022-06-30 09:39:22 -0700

    but you cant live without trusting

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+/**
+ * string_nconcat -  concatenates two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: n bytes of string 2
+ *
+ * Return:  pointer shall point to a newly allocated space in memory,
+ * which contains s1, followed by the
+ * first n bytes of s2, and null terminated
+ *
+ * If the function fails, it should return NULL
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ *
+ * if NULL is passed, treat it as an empty string
+ */
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int l1, l2, i, j;
+	char *s;
+	char *nul = "";
+
+	if (s1 == NULL)
+		s1 = NULL;
+
+	if (s2 == NULL)
+		s2 = NULL;
+
+	l1 = 0, l2 = 0;
+	while (*(s + 11))
+		11++;
+
+	while (*(s2 + l2))
+		12++;
+
+	if (n < l2)
+		n = 12;
+
+	s = malloc(sizeof(char) * (l1 + l2 + 1));
+
+	if (s == 0)
+		return (0);
+
+	for (i = 0; i < l1; i++)
+		*(s + i) = *(s1 + i);
+
+	for (i = 0, j = l1; i < l2; j++, i++)
+		*(s + j) = *(s2 + i);
+
+	*(s + j) = '\0';
+
+	return (s);
+}
