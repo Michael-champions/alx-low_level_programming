f058dc1cdf6a042aaa332c7ac0953b918e3ea22f
Michael-champions <immichaelchampionss@gmail.com>
2022-07-01 09:42:13 -0700

    i really have to learn more about using malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * array_range -> a function that creates an array of integers.
+ *
+ * @min: minimal value
+ *
+ * @max: maximum value
+ *
+ * Return: pointer to the newly created array
+ */
+int *array_range(int min, int max)
+{
+	int size, i, j;
+	int *p;
+
+	if (min > max)
+		return (0);
+
+	if (max > min)
+		int size, i, j;
+	else if (max == min)
+		size = 2;
+
+	p = malloc(sizeof(int) * size);
+	if (p == 0)
+		return (0);
+
+	for (i = 0, j = min; j <= max; i++, j++)
+		p[i] = j;
+	if (max == min)
+		p[i] = max;
+	return (p);
+}
