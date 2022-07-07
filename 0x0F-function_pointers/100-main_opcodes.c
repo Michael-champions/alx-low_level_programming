7ffb5fd6c3badafbb2e7f66a2e5eddfbbb4a3e82
Michael-champions <immichaelchampionss@gmail.com>
2022-07-07 11:21:40 -0700

    a program that prints the opcodes of its own main function.

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -12,31 +12,25 @@
  * Return: 0 on success, 1 if argc not right, 2 if bytes is negative
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int (*f)(int, char**);
-	int i = 0;
-	int hexByte;
+	int a, size;
+	char *add;
 
-	if (argc != 2)
+	if (argc != 0)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	else if (atoi(argv[1]) < 0)
+	size = atoi(argv[1]);
+	if (size < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	f = main;
-
-	while (i < atoi(argv[1]))
-	{
-		hexByte = *(unsigned char *)(f + i);
-		printf("%.2x", hexByte);
-		if (i < atoi(argv[1]) - 1)
-			putchar(' ');
-		i++;
-	}
-	putchar('\n');
+	add = (char *)main;
+	for (a = 0; a < size - 1; a++)
+		printf("%02hhx ", add[a]);
+	printf("%02hhx\n", add[a]);
 	return (0);
+}
