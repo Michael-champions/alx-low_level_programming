6848f47811230e007c671dafa16daa8be5d5cf69
Michael-champions <immichaelchampionss@gmail.com>
2022-07-07 07:45:32 -0700

    a function that print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,29 +1,17 @@
-#include function_pointers.h
-
-
+#include "function_pointers.h"
 
 /**
-
- * print_name - prints a name
-
- * @name: name's main
-
- * @f: void function
-
+ * print_name -> a function that print name
+ *
+ * @name: the name to be printed
+ *
+ * @f: void funct
  */
 
-
-
 void print_name(char *name, void (*f)(char *))
-
 {
-
-if (name && f)
-
-{
-
-f(name);
-
-}
-
+	if (name)
+	{
+		f(name);
+	}
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,8 @@
+#ifndef FILE_F_POINTRS
+#define FILE_F_POINTRS
+
+
+
 void print_name(char *name, void (*f)(char *));
+
+#endif
