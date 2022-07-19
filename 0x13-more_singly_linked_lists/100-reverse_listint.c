2f4d91cbfb0764654053e26ab1060e7c30ac3b43
Michael <99322184+Michael-champions@users.noreply.github.com>
2022-07-19 14:39:37 +0100

    h

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,11 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
-
-
 /**
  * reverse_listint - return sum of list data
  * @head: head node
- *describtion:a function that reverses a listint_t linked list.
  * Return: pointer to first node of reversed list
  */
 listint_t *reverse_listint(listint_t **head)
